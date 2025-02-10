0e9d3be369ac75a1a0094e56138362c2f0ad8f3f
Pham Thanh <thanhlongtivip@gmail.com>
2025-02-10 23:14:04 +0700

    login/register fe

--- src/main/java/com/code/shopee/controller/ShopeeController.java
+++ src/main/java/com/code/shopee/controller/ShopeeController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ShopeeController {
-    @GetMapping("hello")
-    public String Hello() {
-        return "admin/index";
+    @GetMapping("login")
+    public String Login() {
+        return "buyer/login";
+    }
+
+    @GetMapping("register")
+    public String Register() {
+        return "buyer/register";
     }
 }
---
+++ src/main/resources/static/assets/js/login.js
@@ -0,0 +1,51 @@
+
+var normalBox = document.querySelector(".normal-box");
+var loginKey = document.getElementById("login-key");
+
+loginKey.addEventListener("focus", function () {
+    normalBox.classList.add("box-focus");
+});
+
+loginKey.addEventListener("blur", function () {
+    normalBox.classList.remove("box-focus");
+});
+
+var passBox = document.querySelector(".pass-box");
+var password = document.getElementById("password");
+
+password.addEventListener("focus", function() {
+    passBox.classList.add("box-focus");
+});
+
+password.addEventListener("blur", function() {
+    passBox.classList.remove("box-focus");
+});
+
+var passHideBtn = document.getElementById("pass-hide-btn");
+passHideBtn.addEventListener("click", function() {
+    if(password.type === "password") {
+        password.type = "text";
+        passHideBtn.querySelector("i").classList.remove("fa-eye-slash");
+        passHideBtn.querySelector("i").classList.add("fa-eye");
+    }
+    else {
+        password.type = "password";
+        passHideBtn.querySelector("i").classList.remove("fa-eye");
+        passHideBtn.querySelector("i").classList.add("fa-eye-slash");
+    }
+});
+
+var retypePass = document.getElementById("retype-pass");
+var passHideBtn1 = document.getElementById("pass-hide-btn-1");
+passHideBtn1.addEventListener("click", function() {
+    if(retypePass.type === "password") {
+        retypePass.type = "text";
+        passHideBtn1.querySelector("i").classList.remove("fa-eye-slash");
+        passHideBtn1.querySelector("i").classList.add("fa-eye");
+    }
+    else {
+        retypePass.type = "password";
+        passHideBtn1.querySelector("i").classList.remove("fa-eye");
+        passHideBtn1.querySelector("i").classList.add("fa-eye-slash");
+    }
+});
